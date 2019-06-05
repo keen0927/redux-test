3699ba23472dc0f86052072c629f382e3b7a0612
KEEN <kyh0927@live.co.kr>
2019-06-06 00:00:45 +0900

    20190606

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
+
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
