50724ab4855371646c1126d1f46d93a3f4ac02a8
KEEN <kyh0927@live.co.kr>
2019-06-06 00:06:44 +0900

    20190606

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 
 import { createStore } from 'redux';
 import rootReducer from './store/modules';
-
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
+
 console.log(store.getState());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    
+    , document.getElementById('root'));
